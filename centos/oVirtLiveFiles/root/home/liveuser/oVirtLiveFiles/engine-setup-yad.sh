85d029af92d40080b3dcfe862d2baa012c47256e
Ohad Basan <obasan@redhat.com>
2014-02-18 10:51:37 +0200

    ovirt-live: remove output messages from dialog
    Also other cleanups - synced centos/fedora engine-setup-yad.sh
    scripts (hopefully will be merged one day)
    
    Change-Id: Ic67adf2f03491578f70f0c226e2fa9f571a07490
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1056975
    Signed-off-by: Ohad Basan <obasan@redhat.com>
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- centos/oVirtLiveFiles/root/home/liveuser/oVirtLiveFiles/engine-setup-yad.sh
+++ centos/oVirtLiveFiles/root/home/liveuser/oVirtLiveFiles/engine-setup-yad.sh
@@ -2,17 +2,22 @@
 # TODO
 
 gconftool-2 -t str -s /desktop/gnome/background/picture_filename /home/liveuser/oVirtLiveFiles/images/ovirt-wallpaper-16:9.jpg
-sudo ifconfig dummy0 10.0.0.1 netmask 255.255.255.0 > /dev/null
-sudo ifup ovirtmgmt > /dev/null
-sudo brctl addbr ovirtmgmt > /dev/null
-sudo brctl addif ovirtmgmt dummy0 > /dev/null
+if ! ifconfig ovirtmgmt 2> /dev/null | grep -q 'inet addr'; then
+    # network wasn't correctly configured. Attempt to configure.
+    # Probably left over workaround from earlier releases.
+    # TODO - drop this if not needed
+    sudo ifconfig dummy0 10.0.0.1 netmask 255.255.255.0 > /dev/null 2>&1
+    sudo ifup ovirtmgmt > /dev/null 2>&1
+    sudo brctl addbr ovirtmgmt > /dev/null 2>&1
+    sudo brctl addif ovirtmgmt dummy0 > /dev/null 2>&1
+fi
 
 action=$(yad --entry \
     --title "oVirt installation" \
     --text  "oVirt allInOne is going to be installed\nChoose which option you would like to install\nINFO: all passwords on the live system are: \"ovirt\"" \
     --on-top \
     --center\
-    --image=/home/liveuser/rhevLiveFiles/images/ovirt-usb-stick.svg \
+    --image=$HOME/oVirtLiveFiles/images/ovirt-usb-stick.svg \
     --skip-taskbar \
     --image-on-top \
     --listen \
@@ -34,7 +39,7 @@ do_interactive() {
 }
 
 case $action in
-    automatic*) cmd="sudo /usr/bin/engine-setup --offline --config=/home/liveuser/oVirtLiveFiles/ovirt-answer" ;;
+    automatic*) cmd="sudo /usr/bin/engine-setup --offline --config-append=$HOME/oVirtLiveFiles/ovirt-answer" ;;
     interactive*) cmd="do_interactive" ;;
     *) exit 1 ;;
 esac
@@ -42,8 +47,8 @@ esac
 echo "Excecuting $cmd"
 if $cmd; then
     echo "Setup ended successfully"
-    rm -f ~liveuser/.config/autostart/engine-setup.desktop
+    rm -f $HOME/.config/autostart/engine-setup.desktop
     yad --text "Setup ended successfully\nopening oVirt now on https://localhost.localdomain" --button="gtk-ok:0"
-    /usr/bin/firefox https://localhost.localdomain &
+    firefox https://localhost.localdomain &
     exit 0
 fi
--- fedora/oVirtLiveFiles/engine-setup-yad.sh
+++ fedora/oVirtLiveFiles/engine-setup-yad.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 # TODO
 
-sudo ifconfig dummy0 10.0.0.1 netmask 255.255.255.0 > /dev/null
-sudo ifup ovirtmgmt > /dev/null
-sudo brctl addbr ovirtmgmt > /dev/null
-sudo brctl addif ovirtmgmt dummy0 > /dev/null
+if ! ifconfig ovirtmgmt 2> /dev/null | grep -q 'inet addr'; then
+    # network wasn't correctly configured. Attempt to configure.
+    # Probably left over workaround from earlier releases.
+    # TODO - drop this if not needed
+    sudo ifconfig dummy0 10.0.0.1 netmask 255.255.255.0 > /dev/null 2>&1
+    sudo ifup ovirtmgmt > /dev/null 2>&1
+    sudo brctl addbr ovirtmgmt > /dev/null 2>&1
+    sudo brctl addif ovirtmgmt dummy0 > /dev/null 2>&1
+fi
 
 action=$(yad --entry \
     --title "oVirt installation" \
@@ -12,7 +17,7 @@ action=$(yad --entry \
     --on-top \
     --center\
     --text-align=center \
-    --image=/home/oVirtuser/oVirtLiveFiles/images/ovirt-usb-stick.svg \
+    --image=$HOME/oVirtLiveFiles/images/ovirt-usb-stick.svg \
     --skip-taskbar \
     --image-on-top \
     --listen \
@@ -34,17 +39,16 @@ do_interactive() {
 }
 
 case $action in
-    automatic*) cmd="sudo /usr/bin/engine-setup --answer-file=/home/oVirtuser/oVirtLiveFiles/ovirt-answer" ;;
+    automatic*) cmd="sudo /usr/bin/engine-setup --offline --config-append=$HOME/oVirtLiveFiles/ovirt-answer" ;;
     interactive*) cmd="do_interactive" ;;
     *) exit 1 ;;
 esac
 
-echo "excecuting $cmd"
-$cmd
-if [[ $? -eq 0 ]]; then
-    echo "setup ended successfully"
-    rm -f ~oVirtuser/.config/autostart/engine-setup.desktop
-    yad --text "setup ended successfully\nopening oVirt now on https://localhost.localdomain" --button="gtk-ok:0"
-    /bin/firefox https://localhost.localdomain &
+echo "Excecuting $cmd"
+if $cmd; then
+    echo "Setup ended successfully"
+    rm -f $HOME/.config/autostart/engine-setup.desktop
+    yad --text "Setup ended successfully\nopening oVirt now on https://localhost.localdomain" --button="gtk-ok:0"
+    firefox https://localhost.localdomain &
     exit 0
 fi
