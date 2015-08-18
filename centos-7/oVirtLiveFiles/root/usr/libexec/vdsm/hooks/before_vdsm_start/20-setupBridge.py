c95d7919589f9b7ff48e3640b0f43456daffb978
Lev Veyde <lveyde@redhat.com>
2015-08-18 18:33:54 +0300

    centos7: add new bridge setup script
    A new script was added to properly setup the bridge
    
    Change-Id: I8dc802a93b7ec871969e89a5ee96fc199014db30
    Signed-off-by: Lev Veyde <lveyde@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/1222566

---
+++ centos-7/oVirtLiveFiles/root/usr/libexec/vdsm/hooks/before_vdsm_start/20-setupBridge.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('/usr/share/vdsm/')
+from network import api
+import logging
+
+FORMAT = '%(asctime)-15s::%(message)s'
+logging.basicConfig(filename='/var/log/vdsm/setup-vdsm-net.log', format=FORMAT, level=logging.DEBUG)
+
+NOCHK = {'connectivityCheck': False}
+NET = 'ovirtmgmt'
+NIC = 'dummy0'
+
+res = api.setupNetworks({NET: {'nic': NIC, 'ipaddr': '10.0.0.1', 'netmask': '255.255.255.0', 'gateway': '10.0.0.1'}}, {}, **NOCHK)
+logging.debug('setupNetworks: %s', res)
+
+res = api.setSafeNetworkConfig()
+logging.debug('setSafeNetworkConfig: %s', res)
