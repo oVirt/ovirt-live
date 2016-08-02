31d730e5068a4df76627cc6f0fcfd9366d060f62
Sandro Bonazzola <sbonazzo@redhat.com>
2016-08-02 15:49:11 +0200

    fedora: aligned fedora with centos
    - Renamed fedora to fedora-24
    - Aligned fedora-24 content with centos content
    
    Change-Id: I3ae9ddeaf6375b18c472ea83855adf21214387cd
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

---
+++ fedora-24/oVirtLiveFiles/root/usr/libexec/vdsm/hooks/before_vdsm_start/20-setupBridge.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import logging
+import sys
+
+from vdsm.network import api
+
+sys.path.append('/usr/share/vdsm/')
+
+FORMAT = '%(asctime)-15s::%(message)s'
+logging.basicConfig(
+    filename='/var/log/vdsm/setup-vdsm-net.log',
+    format=FORMAT,
+    level=logging.DEBUG
+)
+
+NOCHK = {'connectivityCheck': False}
+NET = 'ovirtmgmt'
+NIC = 'dummy0'
+
+res = api.setupNetworks(
+    {
+        NET: {
+            'nic': NIC,
+            'ipaddr': '10.0.0.1',
+            'netmask': '255.255.255.0',
+            'gateway': '10.0.0.1'
+        }
+    },
+    {},
+    NOCHK
+)
+logging.debug('setupNetworks: %s', res)
+
+res = api.setSafeNetworkConfig()
+logging.debug('setSafeNetworkConfig: %s', res)
